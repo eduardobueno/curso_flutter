083785aee4a5239f03fa86fcff540c2846c6aaab
eduardobueno <eduardobueno@msn.com>
2022-04-04 21:52:50 -0300

    Incluindo AppBar e Body no App

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -6,7 +6,12 @@ class PerguntasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Olá Flutter!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter!'),
+      ),
     );
   }
 }

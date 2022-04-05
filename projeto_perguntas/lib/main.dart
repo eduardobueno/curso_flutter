44068f53a85a0ef4fec28b4768fbc82356db9394
eduardobueno <eduardobueno@msn.com>
2022-04-04 22:02:08 -0300

    Incluídos botões no App

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(PerguntasApp());
+main() => runApp(const PerguntasApp());
 
 class PerguntasApp extends StatelessWidget {
+  const PerguntasApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal de estimação?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: const Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }

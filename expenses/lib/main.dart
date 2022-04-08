a68f329e6ca1e500c36855e5ff2d9d8f9ebbbdf2
eduardobueno <eduardobueno@msn.com>
2022-04-08 13:45:52 -0300

    Acertanto algumas formatações de layout

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 import 'models/transaction.dart';
 
 main() => runApp(const ExpensesApp());
@@ -41,9 +41,9 @@ class MyHomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
+          const SizedBox(
             width: double.infinity,
-            child: const Card(
+            child: Card(
               color: Colors.blue,
               child: Text('Gráfico'),
               elevation: 5,
@@ -55,24 +55,40 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Container(
-                      margin: EdgeInsets.symmetric(
+                      // width: 100,
+                      margin: const EdgeInsets.symmetric(
                         horizontal: 15,
                         vertical: 10,
                       ),
                       decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        tr.value.toString(),
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
                       ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text('R\$ ' + tr.value.toStringAsFixed(2),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          )),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tr.title),
-                        Text(tr.date.toString()),
+                        Text(tr.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Text(
+                          // tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
                       ],
                     ),
                   ],
